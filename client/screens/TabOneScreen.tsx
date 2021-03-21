66381fafcc452010f3a18ff0b21409678610eae2
Amanda Krudde <a.c.krudde@student.tudelft.nl>
2021-03-21 11:46:12 +0100

    login pagina begin

--- client/screens/TabOneScreen.tsx
+++ client/screens/TabOneScreen.tsx
@@ -1,32 +1,116 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-//yay
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React, {useState} from "react";
+import {View, Button, StyleSheet, TouchableOpacity, Text, TextInput} from "react-native";
 
-export default function TabOneScreen() {
+
+// @ts-ignore
+const LoginButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+      <Text style={styles.appButtonText}>{title}</Text>
+    </TouchableOpacity>
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+      <>
+        <UsernameTextBox/>
+        <PasswordTextBox/>
+        <Login/>
+        <CreateProfile/>
+      </>
   );
+};
+
+const UsernameTextBox = () => {
+    const [text, setText] = useState('');
+
+    return (
+        <TextInput
+            style={styles.textContainer}
+            placeholder="Username"
+            onChangeText={text => setText(text)}
+        />
+    );
 }
 
+const PasswordTextBox = () => {
+    const [text, setText] = useState('');
+
+    return (
+        <TextInput
+            style={styles.textContainer}
+            placeholder="Password"
+            onChangeText={text => setText(text)}
+        />
+    );
+};
+
+const Login = () => {
+  const [attemptLogin] = useState(false);
+  return (
+      <View style={styles.screenContainer}>
+        {/*hier kan bij de onpress de actie worden neergezet die uitgevoerd moet worden na het klikken,
+        aka de username en wachtwoord checken*/}
+        <LoginButton onPress={() => {
+          // @ts-ignore
+          attemptLogin(true);
+        }}
+                     title="Log in"/>
+      </View>
+  );
+};
+
+const CreateProfile = () => {
+  const [attemptLogin] = useState(false);
+  return (
+      <View style={styles.screenContainer}>
+        {/*hier kan bij de onpress de actie worden neergezet die uitgevoerd moet worden na het klikken,
+        aka de username en wachtwoord checken*/}
+        <LoginButton onPress={() => {
+          // @ts-ignore
+          attemptLogin(true);
+        }}
+                     title="Create new profile"/>
+      </View>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
+  screenContainer: {
+      justifyContent: 'center',
+      padding: 16
+  },
+  appButtonContainer: {
     justifyContent: 'center',
+    elevation: 8,
+    padding: 16,
+    backgroundColor: "#ad0c00",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    alignSelf: "center",
+    width: 250,
+    marginTop: 20
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  appButtonText: {
+    justifyContent: 'center',
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  textContainer: {
+    justifyContent: 'center',
+    fontSize: 18,
+    padding: 16,
+    backgroundColor: "#d4d4d4",
+    borderRadius: 10,
+    fontWeight: "bold",
+    alignSelf: "center",
+    width: 300,
+    margin: 10,
+    marginTop: 30
+  }
 });
+
+export default App;
